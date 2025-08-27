cd0c6891b25f6c2eb1a16a7d59731609127d7422
Forcng <164341281+Forcng@users.noreply.github.com>
2025-08-26 20:03:20 -0400

    Update svHandler.lua

--- server/svHandler.lua
+++ server/svHandler.lua
@@ -1,14 +1,12 @@
 ESX = exports["es_extended"]:getSharedObject()
 
-for _, gear in pairs(Config.WearableItems) do
-    ESX.RegisterUsableItem(WearableItems.uniqueItem, function(source)
+for _, gear in pairs(Config.Gear) do
+    ESX.RegisterUsableItem(gear.uniqueItem, function(source)
         local xPlayer = ESX.GetPlayerFromId(source)
         if xPlayer then
-            TriggerClientEvent('forcng:applyItem', source, WearableItems)
+            TriggerClientEvent('forcng:applyItem', source, gear)
         end
     end)
 
-    print("usable item:", WearableItems.uniqueItem)
+    print("usable item:", gear.uniqueItem)
 end
-
-
